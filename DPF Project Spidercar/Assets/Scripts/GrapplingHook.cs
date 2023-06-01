8ca35d6dad8d69229bb28309b29ad4c36dffa969
lmcunneen <67307238+lmcunneen@users.noreply.github.com>
2023-06-01 16:59:56 +1000

    Trying to get correct turning
     - still not understanding dot products, so I will fix when I can

--- DPF Project Spidercar/Assets/Scripts/GrapplingHook.cs
+++ DPF Project Spidercar/Assets/Scripts/GrapplingHook.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour
@@ -13,9 +14,12 @@ public class GrapplingHook : MonoBehaviour
 
     float piFloat; //Used for circumference calculations. 
 
-    private RaycastHit2D grapplePointRaycastHit;
     private float grappleRayLength;
     public LayerMask grappleLayers;
+    private Color debugGrappleColour = Color.white;
+
+    float relativePosition;
+    float turnMultiplier; //The float used to make the rotationAngle positive or negative, making it turn correctly for left and right
 
     void Awake()
     {
@@ -32,13 +36,18 @@ public class GrapplingHook : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0)) //When the mouse is pressed down (activating once), find the grapple point
         {
-            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mouseWorldPos.z = 0;
-            var ugh = new Quaternion(0, 0, 0, 1);
-            Instantiate(grapplePointObject, mouseWorldPos, ugh);
-            grapplePointRaycastHit = Physics2D.Raycast(transform.position, mouseWorldPos, grappleRayLength, grappleLayers);
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Finds position of the mouse on the screen and defines a 'transform' variable
+            mouseWorldPos.z = 0; //Makes the point conform to the 2D plane
+            grapplePointObject.transform.position = mouseWorldPos; //Moves the grapple point to where the mouse was clicked
+            RaycastHit2D grapplePointRaycastHit = Physics2D.Raycast(transform.position, grapplePointObject.transform.position, grappleRayLength, grappleLayers);
+            Debug.DrawRay(transform.position, mouseWorldPos, debugGrappleColour, grappleLayers);
+
+            Debug.Log("Raycast hit at this location: " + grapplePointRaycastHit.transform.position);
 
             //grapplePointObject.transform.position = grapplePointRaycastHit.transform.position;
+
+            Vector2 grapplePointDot = grapplePointObject.transform.position - transform.position;
+            relativePosition = Vector2.Dot(Vector2.left, grapplePointDot);
         }
     }
 
@@ -46,6 +55,17 @@ public class GrapplingHook : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Mouse0)) //While the mouse is held down, do the following physics calculations
         {
+
+            if (relativePosition < 0)
+            {
+                turnMultiplier = 1;
+            }
+
+            else
+            {
+                turnMultiplier = -1;
+            }
+
             //Finds rotation angle for the RotateAround function with ***MATHS***
             float distanceRadius = springJoint.distance; //Finds grapple distance by reading the distance variable on the spring joint
             float vehicleVelocity = rb.velocity.magnitude; //Finds current vehicle velocity
@@ -53,25 +73,27 @@ public class GrapplingHook : MonoBehaviour
             float grappleCircumference = 2 * piFloat * distanceRadius; //Finds circumference of turning circle (distance)
             float fullRotationTime = grappleCircumference / vehicleVelocity; //Finds the time it would take to finish the circle. Unsure of what measurement of time it would refer to...
             float segmentTimePerUpdate = fullRotationTime * Time.fixedDeltaTime; //This doesn't work, as it rounds down the number to small, making the result to miniscule. Return to this and fix it
-            float rotationAngle = 360 / (segmentTimePerUpdate * 50); //Determines the angle (still needs more work to accurately find it)
+            float rotationAngle = (360 / (segmentTimePerUpdate * 50)) * turnMultiplier; //Determines the angle (still needs more work to accurately find it) and filters it through the turnMultiplier
 
             //Debug.Log("VELOCITY: " + vehicleVelocity);
             //Debug.Log(grappleCircumference + " / " + vehicleVelocity + " = " + fullRotationTime);
             //Debug.Log(rotationAngle);
 
-            //Handles Rotation Logic
+            //Enables the spring joint and line renderer
             springJoint.enabled = true;
             lineRenderer.enabled = true;
-            Vector3 rotationMask = new Vector3(0, 0, 1);
-            Vector3 point = grapplePointObject.transform.position;
+            //Handles Rotation Logic
+            Vector3 rotationMask = new Vector3(0, 0, 1); //Only rotates on Z axis
+            Vector3 point = grapplePointObject.transform.position; //Assigns the grapple point position to a vector 3 for rotation
             transform.RotateAround(point, rotationMask, Time.fixedDeltaTime * -rotationAngle);
 
-            lineRendererPoints = new Vector3[]  { grapplePointObject.transform.position , gameObject.transform.position };
-            lineRenderer.SetPositions(lineRendererPoints);
+            lineRendererPoints = new Vector3[]  { grapplePointObject.transform.position , gameObject.transform.position }; //Defines the start and end of the line
+            lineRenderer.SetPositions(lineRendererPoints); //Sets the positions to the previously defined positions
         }
 
-        else
+        else //When LMB is let go, do the following...
         {
+            //Disables the spring joint and line renderer
             springJoint.enabled = false;
             lineRenderer.enabled = false;
         }

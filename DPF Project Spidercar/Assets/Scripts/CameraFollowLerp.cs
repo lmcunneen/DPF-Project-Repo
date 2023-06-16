894da9f0cde3136c48473e4eba68d26be7b0cddb
lmcunneen <liam.cunneen282@gmail.com>
2023-06-16 22:01:22 +1000

    Fully Implemented Camera Follow
    Fix (camera) - #4 : Camera now transitions between states, making the camera now fully operational as intended. Values might shift due to testing, but overall the systems are essentially final

--- DPF Project Spidercar/Assets/Scripts/CameraFollowLerp.cs
+++ DPF Project Spidercar/Assets/Scripts/CameraFollowLerp.cs
@@ -11,20 +11,56 @@ public class CameraFollowLerp : MonoBehaviour
      * Still needs to be fully implemented...
      */
 
+    public GameObject vehicle;
     public GameObject objectToFollow;
     [SerializeField] private float zPos;
-    [SerializeField] private float lerpAngle;
+    [SerializeField] private float lerpRateInspector;
+    [SerializeField] private float transitionRate;
+    private float cameraDistance;
+    private bool grappleSuccess;
+    private bool transitionStart;
+    private bool transitionFinish;
+
+    void Start()
+    {
+        transitionRate = lerpRateInspector;
+        transitionStart = true;
+        transitionFinish = false;
+    }
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        grappleSuccess = vehicle.GetComponent<GrapplingHook>().grappleState;
+        
+        if (grappleSuccess)
         {
-            LerpPivotFunction(objectToFollow);
+            transitionStart = true;
+            transitionFinish = false;
+            LerpPivotFunction(objectToFollow, lerpRateInspector);
         }
-
+        
         else
         {
-            PivotFunctionDuplicate(objectToFollow);
+            if (transitionStart)
+            {
+                cameraDistance = Vector2.Distance(transform.position, objectToFollow.transform.position);
+                //Debug.Log("CAMERA DISTANCE = " +  cameraDistance);
+                transitionRate++;
+                LerpPivotFunction(objectToFollow, transitionRate);
+
+                if (cameraDistance < 0.75)
+                {
+                    transitionFinish = true;
+                    transitionStart = false;
+                    transitionRate = lerpRateInspector;
+                }
+            }
+
+            if (transitionFinish)
+            {
+                transitionRate = lerpRateInspector;
+                PivotFunctionDuplicate(objectToFollow);
+            }
         }
     }
 
@@ -38,19 +74,21 @@ public class CameraFollowLerp : MonoBehaviour
         Quaternion pivotRotation = pivot.transform.rotation;
         Quaternion objectRotation = pivotRotation * Quaternion.Euler(0, 0, 1);
         gameObject.transform.rotation = objectRotation;
+        //Debug.Log("Pivot Func Running!");
     }
 
-    void LerpPivotFunction(GameObject pivot)
+    void LerpPivotFunction(GameObject pivot, float lerpRate)
     {
         //Conforms object to the position (linear interpolation)
         Vector3 pivotPosition = Vector3.Scale(pivot.transform.position, new Vector3(1, 1, 0));
         Vector3 currentPosition = gameObject.transform.position;
         pivotPosition.z = -10;
-        gameObject.transform.position = Vector3.Lerp(currentPosition, pivotPosition, lerpAngle * Time.deltaTime);
+        gameObject.transform.position = Vector3.Lerp(currentPosition, pivotPosition, lerpRate * Time.deltaTime);
         currentPosition.z = -10;
         //Conforms object to the rotation (spherical linear interpolation)
         Quaternion pivotRotation = pivot.transform.rotation;
         Quaternion objectRotation = pivotRotation * Quaternion.Euler(0, 0, 1);
-        gameObject.transform.rotation = Quaternion.Slerp(transform.rotation, objectRotation, lerpAngle * Time.deltaTime);
+        gameObject.transform.rotation = Quaternion.Slerp(transform.rotation, objectRotation, lerpRate * Time.deltaTime);
+        //Debug.Log("Lerp Func Running!");
     }
 }
